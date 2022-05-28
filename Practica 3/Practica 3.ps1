dfe399e08515db8e02dede41260e648561db684a
Leviathanser <89880990+Leviathanser@users.noreply.github.com>
2022-05-28 06:20:57 -0500

    Script powershell

---
+++ Practica 3/Practica 3.ps1
@@ -0,0 +1,16 @@
+﻿function tabla1{
+
+    for($i = 1; $i -lt 11; $i++){
+        Write-Host $i 'x' $num '=' ($i*$num)
+    }
+}
+Write-Host "`Tabla de multiplicar"
+Write-Host " Solo numeros positivos"
+[int]$num = [int](Read-Host -Prompt "Escribe un numero: ")
+
+if($num -ge 0){
+    tabla1($num)
+}
+else{
+    Write-Host "El numero es negativo"
+}
